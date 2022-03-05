aadf5dbf226fccf609db5bf1443af623ce013ae4
Shota ABE <abe@lifegram.ltd>
2022-03-05 17:38:47 +0900

    素数列挙

---
+++ prime_2.py
@@ -0,0 +1,10 @@
+print("入力された数までの素数を列挙します")
+num = int(input("いくつまで"))
+print(num, "までの素数は、")
+for i in range(2, num):
+    for j in range(2, i):
+        if i%j == 0:
+            break
+    else:
+        print(i, end=" ")
+print("\nです。")
