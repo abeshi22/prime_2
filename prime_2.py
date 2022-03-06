61c0c82204f8de7c0c448b712bc4f1d81e183e5f
Shota ABE <abe@lifegram.ltd>
2022-03-07 00:06:49 +0900

    fix

--- prime_2.py
+++ prime_2.py
@@ -1,13 +1,15 @@
 print("入力された数までの素数を列挙します")
 while True:
     num = (input("いくつまで"))
-    if num.isdigit() == False:
-        print("正の整数を入力してください")
+    if num.isdigit() == False or int(num) == 0:
+        print("自然数を入力してください")
+    elif int(num) == 1:
+        print("1は素数ではありません")
     else:
         num = int(num)
         break
 
-print(num, "までの素数は、")
+print(num, "より小さい素数は、")
 for i in range(2, num):
     for j in range(2, i):
         if i%j == 0:
