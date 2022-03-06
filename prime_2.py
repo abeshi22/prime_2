cad9c2dc3389610ac7aa1c4af83deae77c429bce
Shota ABE <abe@lifegram.ltd>
2022-03-06 23:35:44 +0900

    数値か判定

--- prime_2.py
+++ prime_2.py
@@ -1,5 +1,10 @@
 print("入力された数までの素数を列挙します")
-num = int(input("いくつまで"))
+num = (input("いくつまで"))
+if num.isdigit() == False:
+    print("数字を入力してください")
+else:
+    num = int(num)
+
 print(num, "までの素数は、")
 for i in range(2, num):
     for j in range(2, i):
